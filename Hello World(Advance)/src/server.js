6678419815584c658f89f2cacf8b9a744223f75b
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-09-20 21:32:24 +0700

    route with dynamic content & ejb

--- Hello World(Advance)/src/server.js
+++ Hello World(Advance)/src/server.js
@@ -1,11 +1,26 @@
 const express = require('express') //import express
+const path = require('path') //import path module
 const app = express() // tạo express application
 const port = 3000  // init port
+
+//config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 //khai báo routes
 //req (request), res(response) là 2 object trong môi trường Node.js
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+  res.send('Hello World with abc')
+})
+
+//with dynamic content route
+app.get('/me', (req, res) => {
+  // res.send('<h1>Hello World with me</h1>')
+  res.render('sample.ejs')
 })
 
 //run server trên port đã khởi tạo trước đấy
