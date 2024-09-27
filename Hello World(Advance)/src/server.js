414811e9dd49fe73081a74599cd2331846fc18dc
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-09-27 15:20:01 +0700

    connection reuse

--- Hello World(Advance)/src/server.js
+++ Hello World(Advance)/src/server.js
@@ -7,7 +7,8 @@ const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 const port = process.env.PORT || 8888// nếu mà không có port thì mặc định là 8888
 const hostname = process.env.HOST_NAME
-const mysql2 = require('mysql2')
+const connection = require('./config/database')
+
 
 
 //config template engine
@@ -17,14 +18,7 @@ configViewEngine(app)
 //khai báo routes
 app.use('/', webRoutes)
 
-//test connection
-const connection = mysql2.createConnection({
-  host:'localhost',
-  port: 3307,
-  user:'root',
-  password:'123456',
-  database:'hoidanit'
-})
+
 
 connection.query('select * from Users', (err, results, field) => {
   console.log(results)
