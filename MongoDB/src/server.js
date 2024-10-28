6e11d5cab0993069aecdde4a923dc6bcd6730227
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-10-28 09:55:37 +0700

    config db

--- MongoDB/src/server.js
+++ MongoDB/src/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const express = require('express'); //commonjs
+const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const connection = require('./config/database');
+const mongoose = require('mongoose');
 
 const app = express();// app express
 const port = process.env.PORT || 8888; //port => hardcode . uat .prod
@@ -18,10 +19,25 @@ configViewEngine(app);
 //khai báo route
 app.use('/', webRoutes);
 
+const kittySchema = new mongoose.Schema({
+    name: String
+});
 
-//test connection
-connection();
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'Hoi dan it Cat' });
 
-app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+cat.save();
+
+(async () => {
+    //test connection
+    try {
+        await connection();
+        app.listen(port, hostname, () => {
+            console.log(`Backend Zero app listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log("err connect to db", error)
+    }
+
+})()
+
